d5c3c3c87587d2f4df92973c5dc49bedb097d700
exysta <lucas.chapart@ensea.fr>
2024-09-16 14:19:15 +0200

    added driver files for motor and accleerometer to cubeide

---
+++ CubeIDE/TagBot/Core/Inc/ADXL343_driver.h
@@ -0,0 +1,13 @@
+/*
+ * ADXL343_driver.h
+ *
+ *  Created on: Sep 16, 2024
+ *      Author: exysta
+ */
+
+#ifndef INC_ADXL343_DRIVER_H_
+#define INC_ADXL343_DRIVER_H_
+
+
+
+#endif /* INC_ADXL343_DRIVER_H_ */
