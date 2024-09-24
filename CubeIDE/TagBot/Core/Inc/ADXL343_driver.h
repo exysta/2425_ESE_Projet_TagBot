410eb9e7b8df64ced32c455f0884f8ac898a3ea0
Marie Caronello <mariecaronello@MacBook-Air-de-Marie-2.local>
2024-09-24 14:09:40 +0200

    update

--- CubeIDE/TagBot/Core/Inc/ADXL343_driver.h
+++ CubeIDE/TagBot/Core/Inc/ADXL343_driver.h
@@ -2,11 +2,32 @@
  * ADXL343_driver.h
  *
  *  Created on: Sep 16, 2024
- *      Author: exysta
+ *      Author: marie
  */
 
 #ifndef INC_ADXL343_DRIVER_H_
 #define INC_ADXL343_DRIVER_H_
+
+
+// register for ADXL343
+#define ADXL343_REG_DEVID 0x00
+#define ADXL343_REG_THRESH_TAP 0x1D
+#define ADXL343_REG_DUR 0X21
+#define ADSL343_REG_THRESH_ACT 0x24
+#define ADXL343_REG_POWER_CTL 0x2D
+#define ADXL343_REG_DATA_FORMAT 0x31
+#define ADXL343_REG_DATAX0 0x32
+#define ADXL343_REG_DATAX1 0x33
+#define ADXL343_REG_DATAY0 0x34
+#define ADXL343_REG_DATAY1 0x35
+#define ADXL343_REG_DATAZ0 0x36
+#define ADXL343_REG_DATAZ1 0x37
+
+// functions for the accelerometer
+void ADXL343_Init(void);
+void ADxl343_Configure(void);
+void ADXL343_ReadRegister(void);
+void ADXL343_DetectTap(void);
 
 
 
--- CubeIDE/TagBot/Core/Src/ADXL343_driver.c
+++ CubeIDE/TagBot/Core/Src/ADXL343_driver.c
@@ -2,7 +2,35 @@
  * ADXL343_driver.c
  *
  *  Created on: Sep 16, 2024
- *      Author: exysta
+ *      Author: marie
  */
 
+#include <ADXL343_driver.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <spi.h>
+#include <stddef.h>
 
+
+
+//Function to initialize the accelerometer
+void ADXL343_Init(void) {
+	uint8_t id;
+	SPI
+}
+
+//Function to configure the accelerometer
+void ADxl343_Configure(void){
+
+}
+
+// Function to read the registers
+void ADXL343_ReadRegister(uint8_t reg, uint8_t* data, size_t length) {
+
+}
+
+//Function to detect a tap
+void ADXL343_DetectTap(void){
+
+}
